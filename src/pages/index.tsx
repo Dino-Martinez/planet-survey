e2217c5bb68ef6d3d4e42d043880e080496cfa9f
Dino-Martinez <dino.martinez.514@gmail.com>
2022-09-15 14:07:46 -0700

    Added form lookup and changed meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  
+  const [slug, setSlug] = useState('');
 
   return (
       <>
-          <div>
+          <div className="flex items-center gap-5 justify-center">
               <Link href="/create">
                   <a className="px-4 py-2 border rounded-md border-slate-100 hover:bg-sky-900">Create a Form</a>
               </Link>
+              <p>OR</p>
+              <div>
+                  <input
+                      type="text"
+                      placeholder="Enter form identifier"
+                      value={slug}
+                      onChange={e => setSlug(e.target.value)}
+                      className="bg-transparent border rounded-md border-slate-100"
+                  />
+                  <Link href={`/forms/${slug}`}>
+                      <a className="ml-3 px-4 py-2 border rounded-md border-slate-100 hover:bg-sky-900">Search</a>
+                  </Link>
+              </div>
           </div>
       </>
   );
